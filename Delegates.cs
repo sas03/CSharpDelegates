2331a30afab0484f63c230df209af96a7ed98774
sas03 <stephanesumo@yahoo.fr>
2023-06-19 09:55:47 +0200

    Delegates

---
+++ Delegates.cs
@@ -0,0 +1,19 @@
+﻿namespace Delegates;
+class Delegates
+{
+    public static double money = 55;
+    public delegate string ShowMoney();
+    public static string Euro(){
+        return money.ToString() + " euros";
+    }
+    public static string Dollar(){
+        return money.ToString() + " euros = " + (money * 1.09).ToString() + " dollars";
+    }
+    static void Main(string[] args)
+    {
+        ShowMoney show = Euro;
+        Console.WriteLine(show());
+        show = Dollar;
+        Console.WriteLine(show());
+    }
+}
